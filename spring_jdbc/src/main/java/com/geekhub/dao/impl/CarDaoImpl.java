9e157005c76a084c6b255572b907a088a9617e7f
ruslan <ruslansholomey@gmail.com>
2017-03-11 19:52:09 +0200

    Spring JDBC

--- spring_jdbc/src/main/java/com/geekhub/dao/impl/CarDaoImpl.java
+++ spring_jdbc/src/main/java/com/geekhub/dao/impl/CarDaoImpl.java
@@ -5,17 +5,13 @@ import com.geekhub.dao.mapper.CarMapper;
 import com.geekhub.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.util.Assert;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 import java.util.List;
 
 @Service
